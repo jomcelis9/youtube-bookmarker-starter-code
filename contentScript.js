0e1acafeb0cf0800c5424d907cfed9116feb6073
Jomari Quiñonez Celis <jomaricelis@gmail.com>
2025-03-13 21:19:38 +0800

    wws

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,5 @@
 (() => {
-    let youtubeLeftControls, youttubePlayer;
+    let youtubeLeftControls, youtubePlayer;
     let currentVideo = "";
 
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
@@ -12,8 +12,25 @@
     });
     const newVideoLoaded = () => {
         const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0]
-        
 
         console.log(bookmarkBtnExists)
+
+        if(!bookmarkBtnExists){
+            const bookmarkBtn = document.createElement("img");
+
+            bookmarkBtn.src - chrome.runtime.getUrl("assets/bookmark.png");
+            bookmarkBtn.className = "ytp-button" + "bookmark-btn";
+            bookmarkBtn.title = "Click to bookmark current timestamp";
+
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+            youtubeLeftControls.appendChild(bookmarkBtn)
+            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler)
+         }
     }
+
+    newVideoLoaded();
 })();
+
+
